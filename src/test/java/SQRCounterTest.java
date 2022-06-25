23dee8f12216de81a728fb9f275ae7a44807ea01
viktor91 <v.a.taradaiko@gmail.com>
2022-06-25 23:34:46 +0400

    Commit 2

--- src/test/java/SQRCounterTest.java
+++ src/test/java/SQRCounterTest.java
@@ -1,14 +1,19 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.sqr.SQRCounterService;
 
 public class SQRCounterTest {
-@Test
+    @Test
     void numberOfSquares() {
         SQRCounterService service = new SQRCounterService();
-        int min = 500;
-        int max = 800;
+        int min = 200;
+        int max = 300;
+        int expected = 3;
         // вычисляем какое количество чисел получилось
-    System.out.println("Количество чисел:" + service.sqrofCount(min, max));
-        }
+        int actual = service.sqrofCount(min, max);
+        //сравниваем результаты
+        Assertions.assertEquals( expected, actual);
+        System.out.println("Количество чисел:" + actual);
+    }
 
 }
--- src/test/java/ru/netology/sqr/SQRCounterService.java
+++ src/test/java/ru/netology/sqr/SQRCounterService.java
@@ -4,7 +4,7 @@ public class SQRCounterService {
     public int sqrofCount(int min, int max) {
         int Sqr = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= min && i * i <= max) {
+            if ((i * i) >= min && (i * i) <= max) {
                 System.out.println(i);
                 Sqr++;
             }
